06152a782e7024ff0d86858a55cda72b280df53d
keyboardsage <keyboardsage@users.noreply.github.com>
2021-07-26 11:50:24 -0400

    Added script that automates adding this repo

--- README.md
+++ README.md
@@ -5,5 +5,7 @@ I am not a fan of having loose files running around my system.
 So this is an APT repo for storing deb files not already in my APT repo.  
 
 # How to use
-1. Make sure you have *git* and *dpkg-dev*
-2. 
+1. Make sure you have *git* and *dpkg-dev* installed
+2. Clone or download this repo
+3. Execute ./sagerepo
+4. Repeat the process periodically (weekly, monthly...whatever), since new debs get added
---
+++ sagerepo.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+APT_REPO_DIR="/opt/sagerepo"
+GIT_REPO_URI="https://github.com/keyboardsage/sagedebs"
+SOURCES_LIST="/etc/apt/sources.list"
+
+# Create a directory for custom deb files
+echo "[*] Creating the repo directory"
+mkdir -p "$APT_REPO_DIR"
+
+# Clone the git repo in that directory
+echo "[*] Cloning the repo of debs"
+if [ "$(ls -A $APT_REPO_DIR/)" ]; then
+	cd "$APT_REPO_DIR"
+	git pull "$GIT_REPO_URI"
+else
+	cd "$APT_REPO_DIR"
+	git clone "$GIT_REPO_URI" .
+fi
+
+# Create a Release file that apt can read
+echo "[*] Creating the Release file"
+dpkg-scanpackages . /dev/null > Release
+
+# Add the new source of debs to the sources.lst if needed
+echo "[*] Adding the repo to the system (if not already present)"
+grep -qxF 'deb [trusted=yes] file://'"$APT_REPO_DIR"' ./' $SOURCES_LIST || echo 'deb [trusted=yes] file://'"$APT_REPO_DIR"' ./' >> $SOURCES_LIST
+
+# Do an apt update to use the repo
+echo "[*] Updating apt to utilize the new repo"
+apt-get update
+
+# Remove the script itself
+echo "[*] Updating apt to utilize the new repo"
+THIS_SCRIPT=`basename "$0"`
+rm "$APT_REPO_DIR/""$THIS_SCRIPT"
