72f5baf2722cd88cba0ddcebf28ec84da03e3805
Keyboard Sage <keyboardsage@users.noreply.github.com>
2024-12-09 08:49:19 -0500

    Update sagerepo.sh
    Updated discord and added vmware horizon client

--- sagerepo.sh
+++ sagerepo.sh
@@ -36,7 +36,8 @@ else
 	wget "https://dl.audiorelay.net/setups/linux/audiorelay-0.27.5.deb"
  	wget "https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb"
 	wget "https://www.expressvpn.works/clients/linux/expressvpn_3.58.0.2-1_amd64.deb"
-	wget "https://dl.discordapp.net/apps/linux/0.0.64/discord-0.0.64.deb"
+	wget "https://dl.discordapp.net/apps/linux/0.0.76/discord-0.0.76.deb"
+        wget "https://download3.omnissa.com/software/CART25FQ2_LIN64_DEBPKG_2406/VMware-Horizon-Client-2406-8.13.0-9995429239.x64.deb"
 fi
 
 # Create a Release/Packages file that apt can read
