1f097582cd0eb677f1efd790c46fd0e21518607f
Keyboard Sage <keyboardsage@users.noreply.github.com>
2024-12-24 03:54:06 -0500

    Update sagerepo.sh
    Solving the known issue by ensuring the opt repo is up-to-date

--- sagerepo.sh
+++ sagerepo.sh
@@ -18,27 +18,29 @@ fi
 echo "[*] Creating the repo directory"
 mkdir -p "$APT_REPO_DIR"
 
-# Clone the git repo in that directory
-echo "[*] Cloning the repo of debs"
-if [ "$(ls -A $APT_REPO_DIR/)" ]; then
-	cd "$APT_REPO_DIR"
-	git pull "$GIT_REPO_URI"
+# Handle the repo directory
+if [ -d "$APT_REPO_DIR" ]; then
+    echo "[*] Cleaning up the existing repo directory"
+    cd "$APT_REPO_DIR"
+    git reset --hard  # Discard uncommitted changes
+    git clean -fdx    # Remove untracked files and directories
+    git pull "$GIT_REPO_URI"
 else
-	cd "$APT_REPO_DIR"
-	git clone "$GIT_REPO_URI" .
-	
-	# large files
-	wget "https://downloads.slack-edge.com/releases/linux/4.31.155/prod/x64/slack-desktop-4.31.155-amd64.deb"
-	wget "https://github.com/PowerShell/PowerShell/releases/download/v7.1.4/powershell_7.1.4-1.ubuntu.20.04_amd64.deb"
-	#wget "https://launchpad.net/openfracas/0.6/0.6.1/+download/openfracas.deb"
-	#wget "http://launchpadlibrarian.net/10780071/libglade2-ruby_0.16.0-10_all.deb"
-	wget "https://az764295.vo.msecnd.net/stable/d037ac076cee195194f93ce6fe2bdfe2969cc82d/code_1.84.0-1698839401_amd64.deb"
-	wget "https://dl.audiorelay.net/setups/linux/audiorelay-0.27.5.deb"
- 	wget "https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb"
-	wget "https://www.expressvpn.works/clients/linux/expressvpn_3.58.0.2-1_amd64.deb"
-	wget "https://dl.discordapp.net/apps/linux/0.0.76/discord-0.0.76.deb"
-        wget "https://download3.omnissa.com/software/CART25FQ2_LIN64_DEBPKG_2406/VMware-Horizon-Client-2406-8.13.0-9995429239.x64.deb"
+    echo "[*] Creating the repo directory"
+    mkdir -p "$APT_REPO_DIR"
+    cd "$APT_REPO_DIR"
+    git clone "$GIT_REPO_URI" .
 fi
+
+# Download additional .deb files
+wget "https://downloads.slack-edge.com/releases/linux/4.31.155/prod/x64/slack-desktop-4.31.155-amd64.deb"
+wget "https://github.com/PowerShell/PowerShell/releases/download/v7.1.4/powershell_7.1.4-1.ubuntu.20.04_amd64.deb"
+wget "https://az764295.vo.msecnd.net/stable/d037ac076cee195194f93ce6fe2bdfe2969cc82d/code_1.84.0-1698839401_amd64.deb"
+wget "https://dl.audiorelay.net/setups/linux/audiorelay-0.27.5.deb"
+wget "https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb"
+wget "https://www.expressvpn.works/clients/linux/expressvpn_3.58.0.2-1_amd64.deb"
+wget "https://dl.discordapp.net/apps/linux/0.0.76/discord-0.0.76.deb"
+wget "https://download3.omnissa.com/software/CART25FQ2_LIN64_DEBPKG_2406/VMware-Horizon-Client-2406-8.13.0-9995429239.x64.deb"
 
 # Create a Release/Packages file that apt can read
 echo "[*] Creating the Release file"
