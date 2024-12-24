4728480241b1f923d5d6bfaf32c88c42e42df25a
keyboardsage <keyboardsage@users.noreply.github.com>
2024-12-24 04:01:23 -0500

    Making sure the repo exists before validating its ina  good state

--- sagerepo.sh
+++ sagerepo.sh
@@ -6,25 +6,25 @@ SOURCES_LIST="/etc/apt/sources.list"
 
 # Sanity check
 if [ "$(id -u)" -ne 0 ]; then
-	echo 'This script must be run by root (sudo)' >&2
-	exit 1
+    echo 'This script must be run by root (sudo)' >&2
+    exit 1
 fi
-
-# Create a new directory for custom deb files
-if [ -d "$APT_REPO_DIR" ]; then
-	echo "[*] Removing the old repo directory"
-	rm -rf "$APT_REPO_DIR"
-fi
-echo "[*] Creating the repo directory"
-mkdir -p "$APT_REPO_DIR"
 
 # Handle the repo directory
 if [ -d "$APT_REPO_DIR" ]; then
     echo "[*] Cleaning up the existing repo directory"
     cd "$APT_REPO_DIR"
-    git reset --hard  # Discard uncommitted changes
-    git clean -fdx    # Remove untracked files and directories
-    git pull "$GIT_REPO_URI"
+    if [ -d ".git" ]; then
+        git reset --hard  # Discard uncommitted changes
+        git clean -fdx    # Remove untracked files and directories
+        git pull "$GIT_REPO_URI"
+    else
+        echo "[*] Directory exists but is not a Git repository. Recreating it."
+        rm -rf "$APT_REPO_DIR"
+        mkdir -p "$APT_REPO_DIR"
+        cd "$APT_REPO_DIR"
+        git clone "$GIT_REPO_URI" .
+    fi
 else
     echo "[*] Creating the repo directory"
     mkdir -p "$APT_REPO_DIR"
@@ -33,6 +33,7 @@ else
 fi
 
 # Download additional .deb files
+echo "[*] Downloading additional .deb files"
 wget "https://downloads.slack-edge.com/releases/linux/4.31.155/prod/x64/slack-desktop-4.31.155-amd64.deb"
 wget "https://github.com/PowerShell/PowerShell/releases/download/v7.1.4/powershell_7.1.4-1.ubuntu.20.04_amd64.deb"
 wget "https://az764295.vo.msecnd.net/stable/d037ac076cee195194f93ce6fe2bdfe2969cc82d/code_1.84.0-1698839401_amd64.deb"
@@ -44,10 +45,9 @@ wget "https://download3.omnissa.com/software/CART25FQ2_LIN64_DEBPKG_2406/VMware-
 
 # Create a Release/Packages file that apt can read
 echo "[*] Creating the Release file"
-#dpkg-scanpackages . /dev/null > Release
 dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
 
-# Add the new source of debs to the sources.lst if needed
+# Add the new source of debs to the sources.list if needed
 echo "[*] Adding the repo to the system (if not already present)"
 grep -qxF 'deb [trusted=yes] file://'"$APT_REPO_DIR"' ./' $SOURCES_LIST || echo 'deb [trusted=yes] file://'"$APT_REPO_DIR"' ./' >> $SOURCES_LIST
 
@@ -57,6 +57,6 @@ apt-get update
 
 # Remove the script/readme etc. files
 echo "[*] Removing unnecessary files from the new apt repo"
-THIS_SCRIPT=`basename "$0"`
-rm "$APT_REPO_DIR/""$THIS_SCRIPT"
-rm "$APT_REPO_DIR/""README.md"
+THIS_SCRIPT=$(basename "$0")
+rm "$APT_REPO_DIR/$THIS_SCRIPT"
+rm "$APT_REPO_DIR/README.md"
